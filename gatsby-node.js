6988f4a0a0465b576a7c62a3778fb4701e029266
Labi Francis <moodyblerd@gmail.com>
2022-11-28 16:22:58 +0100

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,5 @@
+exports.createPages = async ({ graphql, actions }) => {
+	const { createRedirect } = actions;
 createRedirect({
   fromPath: `/products/*`,
   toPath: `https://www.gatsbyjs.com/docs/*`,
